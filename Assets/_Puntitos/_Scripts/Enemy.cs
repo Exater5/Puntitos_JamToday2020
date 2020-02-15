de3fc2ad250074c35af30b1bd8f169ea5c58bdad
torcu <pablotorcuato@gmail.com>
2020-02-15 13:11:44 +0100

    Enemy Finished

--- Assets/_Puntitos/_Scripts/Enemy.cs
+++ Assets/_Puntitos/_Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     Vector2 posObjetivo;
     float x, y;
     [SerializeField]
-    float minX, maxX, minY, maxY, rango;
+    float minX, maxX, minY, maxY, rango, retraso;
     [SerializeField]
     float duration;
     [SerializeField]
@@ -24,6 +24,7 @@ public class Enemy : MonoBehaviour
             transform.position = Vector2.Lerp(transform.position, pObjetivo, aC.Evaluate(i/duration));
             yield return null;
         }
+        yield return new WaitForSeconds(retraso);
         StartCoroutine(Traslada(RandomObjetive()));
     }
 
