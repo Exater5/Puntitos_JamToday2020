ced186257005ab479912b50f7dbe778dddddfb6c
torcu <pablotorcuato@gmail.com>
2020-02-15 11:06:20 +0100

    GameController Basics

--- Assets/_Puntitos/_Scripts/GameController.cs
+++ Assets/_Puntitos/_Scripts/GameController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    float time;
+    bool night;
+    int daysCounter;
+    [SerializeField]
+    float totalDuration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
+        night = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if(time >= totalDuration/2)
+        {
+            night = true;
+            if (time >= totalDuration)
+            {
+                night = false;
+                time = 0;
+                daysCounter++;
+            }
+        }
+
     }
 }
