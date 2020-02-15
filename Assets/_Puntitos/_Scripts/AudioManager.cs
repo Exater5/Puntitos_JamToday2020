9170070e68b85694eaeb4d89cbc609ecda278a1b
torcu <pablotorcuato@gmail.com>
2020-02-15 15:28:19 +0100

    AudioManager

---
+++ Assets/_Puntitos/_Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    public AudioClip[] clips;
+    public AudioSource[] aSources;
+    int currenSource;
+
+    public void PlaySound(int clip)
+    {
+        aSources[currenSource].clip = clips[clip];
+        aSources[currenSource].Play();
+        currenSource++;
+        if(currenSource >= aSources.Length - 1)
+        {
+            currenSource = 0;
+        }
+    }
+}
