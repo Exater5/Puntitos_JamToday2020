7a152d075d56f13b1e04c1f7fe3a7398498cd80e
Jose Torres <33356327+jtorres6@users.noreply.github.com>
2020-02-15 12:04:43 +0100

    Sounds

---
+++ Assets/_Puntitos/_Scripts/ProduceLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProduceLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float timer = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            timer = 0f;
+        }
+        timer+=Time.deltaTime;
+        if(Input.GetMouseButtonUp(1))
+        {
+            Debug.Log(timer);
+            GetComponent<Animator>().SetFloat("ButtonTimer",timer);
+        }
+        else
+        {
+            
+            timer=0f;
+            GetComponent<Animator>().SetFloat("ButtonTimer",timer);
+        }
+    }
+}
