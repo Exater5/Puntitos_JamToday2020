6d4fdabf7872b04531b729a4a3744aff16644a98
Álvaro Fernández García <alvaro89fernandez@gmail.com>
2020-02-15 20:21:15 +0100

    Cristales

--- Assets/_Puntitos/_Scripts/Cristal.cs
+++ Assets/_Puntitos/_Scripts/Cristal.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Cristal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float pickRadio;    
+    PlayerController playerController;
+    GameObject player; 
+    public int points2Spawn;
+    public GameObject _pointReference;
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        playerController = player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if((player.transform.position - transform.position).magnitude < pickRadio)
+        {
+            Picked();
+        }
+    }
+
+    void Picked()
+    {
+        for(int i=0; i<points2Spawn; ++i)
+        {
+            playerController._points.Add(Instantiate(_pointReference, player.transform.position, Quaternion.identity));
+        }
+        Destroy(gameObject);
     }
 }
