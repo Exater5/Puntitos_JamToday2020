10c574cb29263d0b3b16a7603f979fdf3a724bdd
torcu <pablotorcuato@gmail.com>
2020-02-15 18:41:14 +0100

    Recursos UI

---
+++ Assets/_Puntitos/_Scripts/RecogeRecursos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecogeRecursos : MonoBehaviour
+{
+    GameController gc;
+    public GameObject prefabRecurso;
+    public List<GameObject> recursos;
+    float offset = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gc = FindObjectOfType<GameController>();
+        for(int i = 0; i <= gc.bolitasRestantes; i++)
+        {
+            GameObject nuevoRecurso = Instantiate(prefabRecurso, transform.position + new Vector3(offset * i,0,0), Quaternion.identity);
+            recursos.Add(nuevoRecurso);
+        }
+    }
+    public void RecogeRecurso()
+    {
+        Destroy(recursos[recursos.Count-1]);
+        recursos.RemoveAt(recursos.Count - 1);
+    }
+}
