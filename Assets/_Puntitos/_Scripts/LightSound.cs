ea67e6d71e0b38de1ede948dba582cdc4e15a22a
Jose Torres <33356327+jtorres6@users.noreply.github.com>
2020-02-16 13:01:05 +0100

    LightSound

---
+++ Assets/_Puntitos/_Scripts/LightSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSound : MonoBehaviour
+{
+    AudioManager aM;
+    void Start()
+    {
+        aM = FindObjectOfType<AudioManager>();
+    }
+    public void PlaySound(int sonido)
+    {
+        aM.PlaySound(sonido);
+    }
+
+}
