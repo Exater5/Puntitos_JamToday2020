33dd3121950e7a96864a4a94b4c43ebfb12e0593
Jose Torres <33356327+jtorres6@users.noreply.github.com>
2020-02-16 11:32:00 +0100

    Post pro y particulas

--- Assets/_Puntitos/_Scripts/Die.cs
+++ Assets/_Puntitos/_Scripts/Die.cs
@@ -6,6 +6,9 @@ public class Die : MonoBehaviour
 {
     GameObject cam;
 
+    public GameObject blood;
+    public ParticleSystem explosion;
+
     void Update()
     {
         cam = Camera.main.gameObject;
@@ -19,6 +22,8 @@ public class Die : MonoBehaviour
             FindObjectOfType<GameController>().deadPositions.Add(other.transform.position);
             cam.GetComponent<CameraShake>().shakeDuration=0.01f;
             GameObject.Find("Player").GetComponent<PlayerController>()._points.Remove(gameObject);
+            explosion.Play();
+            Instantiate(blood,transform.position,Quaternion.Euler(Random.Range(0f,360f),Random.Range(0f,360f),0f));
             Destroy(gameObject);
         }
     }
