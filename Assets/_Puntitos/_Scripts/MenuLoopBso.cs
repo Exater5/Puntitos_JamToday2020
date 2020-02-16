705dec5769c4272cca9b0a062c671c0b2a4b9924
torcu <pablotorcuato@gmail.com>
2020-02-16 10:46:56 +0100

    Menu and Main Scene Fx

---
+++ Assets/_Puntitos/_Scripts/MenuLoopBso.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLoopBso : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource bso1, bso2;
+    bool played = false;
+    // Update is called once per frame
+    void Update()
+    {
+        print(bso1.time);
+        if (played) { print("AHORA"); }
+        if (bso1.time >= 30 && !played)
+        {
+            played = true;
+            bso2.Play();
+        }
+    }
+}
