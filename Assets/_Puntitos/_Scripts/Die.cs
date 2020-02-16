020d711ecc0ca159cdea8091d40ddad6628ffaa4
Álvaro Fernández García <alvaro89fernandez@gmail.com>
2020-02-16 09:28:46 +0100

    Eliminando el timescale en die

--- Assets/_Puntitos/_Scripts/Die.cs
+++ Assets/_Puntitos/_Scripts/Die.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class Die : MonoBehaviour
 {
-
-    float originalScale;
-
     GameObject cam;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
         cam = Camera.main.gameObject;
     }
 
@@ -27,23 +17,9 @@ public class Die : MonoBehaviour
         {
             FindObjectOfType<GameController>().deadCounter++;
             FindObjectOfType<GameController>().deadPositions.Add(other.transform.position);
-            originalScale = Time.timeScale;
             cam.GetComponent<CameraShake>().shakeDuration=0.01f;
-            //Time.timeScale = 0.05f;
             GameObject.Find("Player").GetComponent<PlayerController>()._points.Remove(gameObject);
             Destroy(gameObject);
-            //Time.timeScale = originalScale;
         }
-    }
-
-    IEnumerator Wait()
-    {
-        for(float i=0; i<=0.01f; i+=Time.unscaledDeltaTime)
-        {
-            yield return null;
-            
-
-        }
-        Time.timeScale = originalScale;
     }
 }
