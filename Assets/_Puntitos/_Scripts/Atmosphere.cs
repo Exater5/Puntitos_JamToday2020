31630263c883ad4e593642e470ae0cf11b951d9f
Jose Torres <33356327+jtorres6@users.noreply.github.com>
2020-02-16 11:03:50 +0100

    Postpro

---
+++ Assets/_Puntitos/_Scripts/Atmosphere.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Atmosphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameController controller;
+
+    public bool boolean = true;
+    void Start()
+    {
+        controller = FindObjectOfType<GameController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.night == boolean)
+        {
+            gameObject.GetComponent<Volume>().weight = 1f;
+          
+        }
+        else
+        {
+             gameObject.GetComponent<Volume>().weight = 0f;
+        }
+    }
+}
